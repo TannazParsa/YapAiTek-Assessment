1d6a870dc98b949f5f8d262a6e01e18e1ddba2c0
Tannaz <tannaz.parsa69@gmail.com>
2020-12-18 20:36:40 +0330

    initial commit

---
+++ YapAiTek-Assessment/YapAiTek-Assessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YapAiTek-Assessment
+//
+//  Created by tanaz on 9/28/1399 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
