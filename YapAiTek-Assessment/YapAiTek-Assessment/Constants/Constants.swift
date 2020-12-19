948ef2cdc176e6ee2c3fea45924c64e59a91b742
Tannaz <tannaz.parsa69@gmail.com>
2020-12-19 11:35:35 +0330

    [upd] flickrServices

--- YapAiTek-Assessment/YapAiTek-Assessment/Constants/Constants.swift
+++ YapAiTek-Assessment/YapAiTek-Assessment/Constants/Constants.swift
@@ -11,6 +11,7 @@ struct Constants {
     
    static var shared = Constants()
     
+    var baseURL          = "https://api.flickr.com/services/rest/"
     var api_key          = "aeea051fec6eda5ed5b31cc52780880e"
     var secretKey        = "069c67b7bec8c4a4"
     var requestTokenUrl  = "https://www.flickr.com/services/oauth/request_token"
--- YapAiTek-Assessment/YapAiTek-Assessment/Network/Services/FlickrServices.swift
+++ YapAiTek-Assessment/YapAiTek-Assessment/Network/Services/FlickrServices.swift
@@ -11,20 +11,24 @@ import RxSwift
 import RxCocoa
 import Alamofire
 
+enum FlickrMethods:String {
+   case recentPhotos            =  "flickr.photos.getRecent"
+}
+
 struct FlickrServices {
     
     static let shared = FlickrServices()
     
     func getSearchPhotos(page:Int,consumerKey: String) -> Observable<Result<FlickrSearchResponse>>  {
         let parameters :Dictionary = [
-            "method"         : "flickr.photos.getRecent",
+            "method"         : FlickrMethods.recentPhotos.rawValue,
             "api_key"        : consumerKey,
             "format"         : "json",
             "nojsoncallback" : "1",
             "extras"         : "url_q,url_z",
             "page"           : page
         ] as [String : Any]
-        return WebServices.shared.request(url: "https://api.flickr.com/services/rest/",
+        return WebServices.shared.request(url: Constants.shared.baseURL,
                                           param: parameters,
                                           method: .get,
                                           encoding: URLEncoding.default)
